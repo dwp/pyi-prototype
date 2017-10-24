e979fe830cee9f2a29500f1eb95fe4e92c4ec609
Gary Boyle <gary.boyle@dwp.gsi.gov.uk>
2017-10-24 15:23:02 +0100

    Update service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Universal Credit',
+  serviceName: 'Prove your Identity',
 
   // Default port that prototype runs on
   port: '3000',
